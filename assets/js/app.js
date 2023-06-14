750938c0727df0538f9d9b27e66a5cf86451fd42
sinisterSensation <bayukov.kirya@mail.ru>
2023-06-14 19:16:05 +0300

    second commit/ new pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
-let chat = document.getElementById('chat');
 const section0 = document.getElementById('section0');
+const section1 = document.getElementById('section1');
+const section2_0 = document.getElementById('section2_0');
+const section3 = document.getElementById('section3');
 const section4 = document.getElementById('section4');
+
+let chat = document.getElementById('chat');
+let pageTitle = document.getElementById('pageTitle')
 
 function openChat() {
     chat.style = 'display: block;';
@@ -11,12 +16,47 @@ function closeChat() {
 }
 
 function openMainPage() {
+    pageTitle.innerText = 'Чебоксарский завод металлоизделий';
     section0.style = 'display: block;';
+    section1.style = 'display: none;';
+    section2_0.style = 'display: none;';
+    section3.style = 'display: none;';
+    section4.style = 'display: none;';
+}
+
+function openDeliverPage() {
+    pageTitle.innerText = 'Доставка';
+    section0.style = 'display: none;';
+    section1.style = 'display: block;';
+    section2_0.style = 'display: none;';
+    section3.style = 'display: none;';
+    section4.style = 'display: none;';
+}
+
+function openSection2_0() {
+    pageTitle.innerText = 'Лазерная резка металла';
+    section0.style = 'display: none;';
+    section1.style = 'display: none;';
+    section2_0.style = 'display: block;';
+    section3.style = 'display: none;';
+    section4.style = 'display: none;';
+}
+
+function openAboutPage() {
+    pageTitle.innerText = 'О компании';
+    section0.style = 'display: none;';
+    section1.style = 'display: none;';
+    section2_0.style = 'display: none;';
+    section3.style = 'display: block;';
     section4.style = 'display: none;';
 }
 
 function openContactsPage() {
+    pageTitle.innerText = 'Контакты';
     section0.style = 'display: none;';
+    section1.style = 'display: none;';
+    section2_0.style = 'display: none;';
+    section3.style = 'display: none;';
     section4.style = 'display: block;';
 }
 
